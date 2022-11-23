d11799d68d7519993226dad5bac877e84287ead5
hrmk1o3 <20158457+hrmk1o3@users.noreply.github.com>
2022-11-23 15:39:36 +0400

    Modify RequestTxBroadcastBody

--- src/interface/mod.rs
+++ src/interface/mod.rs
@@ -90,6 +90,7 @@ pub struct ResponseTxSendBody {
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct RequestTxBroadcastBody {
     pub signer_address: Address<F>,
+    pub tx_hash: WrappedHashOut<F>,
     pub purge_output_inclusion_witnesses: Vec<SmtInclusionProof<F>>,
     pub assets: Vec<Vec<Asset<F>>>,
 }
