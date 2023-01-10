dac04837290b2e565331f72fc06b00fda4f326a5
hrmk1o3 <20158457+hrmk1o3@users.noreply.github.com>
2023-01-10 21:17:01 +0400

    remove pub keyword

--- src/constants.rs
+++ src/constants.rs
@@ -1,21 +1,21 @@
 use intmax_zkp_core::config::RollupConstants;
 
-pub const LOG_MAX_N_USERS: usize = 16;
-pub const LOG_MAX_N_TXS: usize = 24;
-pub const LOG_MAX_N_CONTRACTS: usize = LOG_MAX_N_USERS;
-pub const LOG_MAX_N_VARIABLES: usize = 8;
-pub const LOG_N_TXS: usize = 4;
-pub const LOG_N_RECIPIENTS: usize = LOG_MAX_N_USERS;
-pub const LOG_N_CONTRACTS: usize = LOG_MAX_N_CONTRACTS;
-pub const LOG_N_VARIABLES: usize = LOG_MAX_N_VARIABLES;
-pub const N_REGISTRATIONS: usize = 16;
-pub const N_DEPOSITS: usize = 16;
-pub const N_MERGES: usize = 16;
-pub const N_DIFFS: usize = 16;
-pub const N_BLOCKS: usize = 4;
+const LOG_MAX_N_USERS: usize = 16;
+const LOG_MAX_N_TXS: usize = 24;
+const LOG_MAX_N_CONTRACTS: usize = LOG_MAX_N_USERS;
+const LOG_MAX_N_VARIABLES: usize = 8;
+const LOG_N_TXS: usize = 4;
+const LOG_N_RECIPIENTS: usize = LOG_MAX_N_USERS;
+const LOG_N_CONTRACTS: usize = LOG_MAX_N_CONTRACTS;
+const LOG_N_VARIABLES: usize = LOG_MAX_N_VARIABLES;
+const N_REGISTRATIONS: usize = 16;
+const N_DEPOSITS: usize = 16;
+const N_MERGES: usize = 16;
+const N_DIFFS: usize = 16;
+const N_BLOCKS: usize = 4;
 
 /// block number の最大値の対数
-pub const N_LOG_MAX_BLOCKS: usize = 32;
+pub const LOG_MAX_N_BLOCKS: usize = 32;
 
 pub const ROLLUP_CONSTANTS: RollupConstants = RollupConstants {
     log_max_n_users: LOG_MAX_N_USERS,
