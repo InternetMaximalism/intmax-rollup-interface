a6730ffd30e823685b79ae4971c06e15b10c0e32
hrmk1o3 <20158457+hrmk1o3@users.noreply.github.com>
2023-05-10 21:35:35 +0200

    modify network config

--- src/constants.rs
+++ src/constants.rs
@@ -71,7 +71,7 @@ pub const LOCAL_NETWORK_CONFIG: ContractConfig = ContractConfig {
     rpc_url: "http://localhost:8545",
     explorer_url: None,
     chain_id: 31337,
-    verifier_contract_address: "0x610178dA211FEF7D417bC0e6FeD39F05609AD788",
+    verifier_contract_address: "0x0165878A594ca255338adfa4d48449f69242Eb8F",
     offer_manager_contract_address: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
     reverse_offer_manager_contract_address: "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9",
 };
